0f56fdd01e64e3b30ff1b5472180da890b89229b
Vladimir Gorin <vladangor@gmail.com>
2019-11-10 10:18:06 +0300

    1.try with resources 2. eliminate cycle

--- src/main/java/vladimir/gorin/Server.java
+++ src/main/java/vladimir/gorin/Server.java
@@ -13,90 +13,66 @@ import java.util.stream.Collectors;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket(39877);
-        System.out.println("Got port");
-        while (true) {
-
-            Socket soc = ss.accept();
-
+    public static void main(String[] args) {
+        String body;
+        String header;
+        StringBuilder line;
+        String text;
+        try (ServerSocket ss = new ServerSocket(39877);
+             Socket soc = ss.accept();
+             BufferedReader br = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()))) {
+            System.out.println("Got port");
             System.out.println("Client exist!");
+            line = new StringBuilder();
 
+            while ((!(text = Optional.ofNullable(br.readLine()).orElse("")).equals(""))) {
+                line.append(text);
+            }
 
-            InputStream is = soc.getInputStream();
-            OutputStream os = soc.getOutputStream();
-
-            InputStreamReader isr = new InputStreamReader(is);
-            OutputStreamWriter osw = new OutputStreamWriter(os);
-
-            BufferedReader br = new BufferedReader(isr);
-            BufferedWriter bw = new BufferedWriter(osw);
-            String body;
-            String header;
-
-
-//            while (soc.isConnected()) {
-                StringBuilder line = new StringBuilder();
-                String text;
-                try {
-
-                   while ((!(text=Optional.ofNullable(br.readLine()).orElse("")).equals(""))){
-                       line.append(text);
-                   }
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                    break;
-                }
-                if (line.indexOf("GET")!=-1) {
-                    try {
-                        //files directory
-                        body=getBody();
-                        header=getHTTPHeader(body.length(),true);
-                        bw.write(header+body);
-                        bw.newLine();
-                        bw.flush();
-
-
-                    } catch (SocketException ex) {
-                        ex.printStackTrace();
-                        break;
-                    }
-                } else {
-                    bw.write(getHTTPHeader(0,false));
-                    bw.flush();
-                }
-
-//           }
-
-            bw.close();
-            br.close();
-            soc.close();
+            if (line.indexOf("GET") != -1) {
+                body = getBody();
+                header = getHTTPHeader(body.length(), true);
+                bw.write(header + body);
+                bw.flush();
+            } else {
+                bw.write(getHTTPHeader(0, false));
+                bw.flush();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
-    private static List<String> getListFiles(){
-        ArrayList<String> arr = new ArrayList<>();
+
+
+    private static List<String> getListFiles() {
+        ArrayList<String> arr = new ArrayList<String>(){
+            {
+               add("");
+            }
+        };
         String absolutePath = new File(".").getAbsolutePath();
         File file = new File(absolutePath);
         File[] files = file.listFiles();
-        return Arrays.stream(files).map(File::getName).collect(Collectors.toList());
+        return files==null?arr:Arrays.stream(files).map(File::getName).collect(Collectors.toList());
     }
 
-    private static String getBody(){
-        StringBuilder stringBuilder=new StringBuilder();
+    private static String getBody() {
+        StringBuilder stringBuilder = new StringBuilder();
 
         stringBuilder.append("<html><body><h1>Hello ").append(System.getProperty("user.name")).append("! List of files in the directory<ul>\n");
-        for (String str: getListFiles()) {
+        for (String str : getListFiles()) {
             stringBuilder.append(" <li>").append(str).append("</li>\n");
         }
         stringBuilder.append("</ul></h1></body></html>");
         return stringBuilder.toString();
     }
 
-    private static String getHTTPHeader(int length, boolean isOk){
+    private static String getHTTPHeader(int length, boolean isOk) {
         String stOK = "HTTP/1.1 200 OK";
         String stNotOK = "HTTP/1.1 404 Not Found";
 
-        return (isOk?stOK:stNotOK)+ "\r\n" +
+        return (isOk ? stOK : stNotOK) + "\r\n" +
                 "Server: YarServer/2009-09-09\r\n" +
                 "Content-Type: text/html;charset=utf-8\r\n" +
                 "Content-Length: " + length + "\r\n" +
