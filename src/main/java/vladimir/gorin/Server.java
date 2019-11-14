6c01230610a45811dc415af7a2a48bcfcdd9743e
Vladimir Gorin <vladangor@gmail.com>
2019-11-14 15:11:31 +0300

    1.add cycle while for server 2. add emptyList construction

--- src/main/java/vladimir/gorin/Server.java
+++ src/main/java/vladimir/gorin/Server.java
@@ -4,10 +4,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -18,39 +15,44 @@ public class Server {
         String header;
         StringBuilder line;
         String text;
-        try (ServerSocket ss = new ServerSocket(39877);
-             Socket soc = ss.accept();
-             BufferedReader br = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()))) {
-            System.out.println("Got port");
-            System.out.println("Client exist!");
-            line = new StringBuilder();
 
-            while ((!(text = Optional.ofNullable(br.readLine()).orElse("")).equals(""))) {
-                line.append(text);
-            }
+        while (true){
+            try (ServerSocket ss = new ServerSocket(39877);
+                 Socket soc = ss.accept()){
+                try (
+                        BufferedReader br = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()))) {
+                    System.out.println("Got port");
+                    System.out.println("Client exist!");
+                    line = new StringBuilder();
 
-            if (line.indexOf("GET") != -1) {
-                body = getBody();
-                header = getHTTPHeader(body.length(), true);
-                bw.write(header + body);
-                bw.flush();
-            } else {
-                bw.write(getHTTPHeader(0, false));
-                bw.flush();
+                    while ((!(text = Optional.ofNullable(br.readLine()).orElse("")).equals(""))) {
+                        line.append(text);
+                    }
+
+                    if (line.indexOf("GET") != -1) {
+                        body = getBody();
+                        header = getHTTPHeader(body.length(), true);
+                        bw.write(header + body);
+                        bw.flush();
+                    } else {
+                        bw.write(getHTTPHeader(0, false));
+                        bw.flush();
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
         }
+
     }
 
 
     private static List<String> getListFiles() {
-        ArrayList<String> arr = new ArrayList<String>(){
-            {
-               add("");
-            }
-        };
+        List<String> arr =(List<String>)Collections.EMPTY_LIST;
         String absolutePath = new File(".").getAbsolutePath();
         File file = new File(absolutePath);
         File[] files = file.listFiles();
